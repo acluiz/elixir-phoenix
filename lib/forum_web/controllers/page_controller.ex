aab975a927b8c2f8242a22b63347f379c3c90ee6
Ana Luiz <acluiz.study@gmail.com>
2026-09-10 09:50:25 -0300

    add routes

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,17 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  @spec users(Plug.Conn.t(), any()) :: Plug.Conn.t()
+  def users(conn, _params) do
+    users = [
+      %{ id: 1, name: "Alice", email: "alice@mail.com"},
+      %{ id: 1, name: "Bob", email: "bob@mail.com"},
+      %{ id: 1, name: "John", email: "john@mail.com"},
+      %{ id: 1, name: "Julia", email: "julia@mail.com"}
+    ]
+
+    # render(conn, :users, users: users)
+    json(conn, %{ users: users })
+  end
 end
